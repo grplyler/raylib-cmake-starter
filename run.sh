299a7d1e728ba5f8923a6032b2d5892d247ffa13
Ryan Plyler <g.r.plyler@gmail.com>
2023-04-01 14:50:02 -0400

    rename build and run scripts

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export ANGLE_LIBRARY_PATH=/Users/ryan/code/projects/raylib-cmake-starter/vendor/angle/out/Release
-export DYLD_LIBRARY_PATH=${ANGLE_LIBRARY_PATH}:${DYLD_LIBRARY_PATH}
-
-./build/raylib-starter
