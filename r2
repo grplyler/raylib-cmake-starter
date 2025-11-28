46e47fd037cbed0b689edad44f66034eb96f6f28
Ryan Plyler <g.r.plyler@gmail.com>
2025-11-27 17:36:24 -0800

    add rglayout to the starter, get rid of enet

--- r2
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run two instances of the raylib-starter project
-
-./build/raylib-starter-enet &
-./build/raylib-starter-enet &
--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,89 @@
-
-#include "raylib.h"   
-
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
+#define RGLAYOUT_IMPLEMENTATION
+#include "rglayout.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <cstring>
+#include <cstdio>
+
+// Helper function to draw debug buttons (similar to Odin's DebugButtonEx)
+static void DebugButton(Rectangle rect, const char* label) {
+    // Draw button with distinctive colors for debugging layout
+    static int button_counter = 0;
+    char button_text[64];
+
+    if (label != NULL) {
+        strncpy(button_text, label, sizeof(button_text) - 1);
+    } else {
+        std::snprintf(button_text, sizeof(button_text), "Button %d", button_counter++);
+    }
+
+    GuiButton(rect, button_text);
+}
+
 //------------------------------------------------------------------------------------
-// Program main entry point
+// Sidebar Settings Demo
 //------------------------------------------------------------------------------------
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(720, 480, "RayGUI Layout - 3Panel Demo");
+    SetTargetFPS(60);
 
-    // Variables
-    bool showTextInputBox = false;
-    bool checked = false;
+    // Load GUI style - try multiple possible paths
+    GuiLoadStyle("vendor/raygui/styles/cyber/style_cyber.rgs");
 
-    InitWindow(screenWidth, screenHeight, "Raylib CMake Starter");
-    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-    SetWindowState(FLAG_WINDOW_RESIZABLE);
-    //--------------------------------------------------------------------------------------
+    // GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
+    Color bg_color = GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR));
 
-    // GUI: Initialize gui parameters
-    // GuiLoadStyle("/Users/ryan/code/projects/raylib-cmake-starter/vendor/raygui/styles/cyber/cyber.rgs");
-    GuiLoadStyle("/Users/ryan/code/projects/raylib-cmake-starter/vendor/raygui/styles/jungle/jungle.rgs");
+    // Layout Settings
+    RGLSetDefaultGap(10.0f);
+    RGLSetDefaultPadAll(0.0f);
 
     // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        if (IsKeyPressed(KEY_Q)) break;
+
+        // Update (nothing yet)
 
         // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
+        ClearBackground(bg_color);
 
-        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+        float screen_w = (float)GetScreenWidth();
+        float screen_h = (float)GetScreenHeight();
+        Rectangle panel = {0, 0, screen_w, screen_h};
 
-        if (GuiButton((Rectangle){ 25, 255, 125, 30 }, "Push me!")) {
-            printf("Button clicked!\n");
-        }
+        // Toplevel Column: Header(50px), Content(stretch), Footer(50px)
+        RGLPlan column_plan = GuiPlanCreate((float[]){50, -1}, 2);
+        GuiPlanSetPadAll(&column_plan, 10);
+        GuiBeginColumn(panel, &column_plan);
 
-        // Add a checkbox
-        GuiCheckBox((Rectangle){ 25, 290, 20, 20 }, "Check me", &checked);
+            // Header Row
+             DebugButton(GuiLayoutRec(-1, -1), "Header");
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            // Content Row with 3 columns: Left(flex 1), Center(flex 3), Right(flex 1)
+            RGLPlan content_plan = GuiPlanCreate((float[]){1, 3, 1}, 3);
+            GuiBeginRow(GuiLayoutRec(-1, -1), &content_plan);
+                DebugButton(GuiLayoutRec(-1, -1), "Left\nSidebar");      // Left Sidebar
+                DebugButton(GuiLayoutRec(-1, -1), "Main\nContent");      // Center Content
+                DebugButton(GuiLayoutRec(50, -1), "Right\nSidebar");;
+            GuiLayoutEnd();
+
+
+        GuiLayoutEnd(); // Toplevel Column
+
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
-    //--------------------------------------------------------------------------------------
     CloseWindow(); // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
     return 0;
 }
